bfe58c8e1ffb47dd6e32a1b2287c51eb1d39a7d5
tienduong-java <67788127+tienduong-java@users.noreply.github.com>
2020-07-10 22:18:03 +0700

    Đặng Tiến Dương nộp bt1 4/7/2020

--- solutions/04_07_2020/bai1/Bai1.java
+++ solutions/04_07_2020/bai1/Bai1.java
@@ -1,43 +1,52 @@
-
-package javaapplication1;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BaiTapJava2;
 
 import java.util.Scanner;
 
+/**
+ *
+ * @author Admin
+ */
 public class Bai1 {
+    //Giải phương trình bậc 2
+
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Nhap a: ");
-        double a = input.nextInt();
-        System.out.print("Nhap b: ");
-        double b = input.nextInt();
-        System.out.print("Nhap c: ");
-        double c = input.nextInt();
-        if(a==0) {
-            if(b==0) {
-                if(c==0) {
-                    System.out.println("Phuong trinh co vo so nghiem");
-                }
-                else {
-                    System.out.println("Phuong trinh vo nghiem");
-                }
-            }
-            else {
-                System.out.println("Phuong trinh co nghiem duy nhat la: " + (-c/b));
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Nhập hệ số a: ");
+        float a= sc.nextInt();
+        System.out.println("Nhập hệ số b: ");
+        float b=sc.nextInt();
+        System.out.println("Nhập hệ số c: ");
+        float c=sc.nextInt();
+        if (a==0){
+            if(b==0){
+                System.out.println("Phương trình vô nghiệm");
+            }else{
+                System.out.println("Phương trình có nghiệm là: "+(-c)/b);
             }
         }
-        else {
-            double d = b*b - 4*a*c;
-            if (d<0) {
-                System.out.println("Phuong trinh vo nghiem");
-            }
-            else if (d==0) {
-                System.out.println("Phuong trinh co nghiem kep la: " + (-b/(2*a)));
-            }
-            else {
-                System.out.println("Phuong trinh co 2 nghiem phan biet la:");
-                System.out.println("x1 = " +((-b+ Math.sqrt(d))/(2*a)));
-                System.out.println("x2 = " +((-b- Math.sqrt(d))/(2*a)));
-            }
+        double delta=b*b -4*a*c;
+        double x1;
+        double x2;
+        if (delta>0){
+            System.out.println("Phương trình có 2 nghiệm phân biệt là:");
+            System.out.println("x1="+(double)((-b)+Math.sqrt(delta))/2*a);
+            System.out.println("x2="+(double)((-b)-Math.sqrt(delta))/2*a);
+        } else if(delta==0){
+            System.out.println("Phương trình có nghiệm kép");
+            System.out.println("x1=x2= " +(-b)/(2*a));
+        } else{
+            System.out.println("Phương trình vô nghiệm");
         }
+        
+            
     }
+    
 }
