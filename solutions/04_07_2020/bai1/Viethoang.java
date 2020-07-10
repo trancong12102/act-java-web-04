fa3035a155017a31f07cd2b338f74caa3d567624
Zysd836 <67743418+Zysd836@users.noreply.github.com>
2020-07-10 17:27:04 +0700

    Add files via upload

--- solutions/04_07_2020/bai1/Viethoang.java
+++ solutions/04_07_2020/bai1/Viethoang.java
@@ -3,68 +3,60 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package docso;
+package test2;
 
 import java.util.Scanner;
+
+
 
 /**
  *
  * @author vieth
  */
-public class Docso {
+public class Test2 
+ {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-      int number;
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Nhap so:");
-        number=sc.nextInt();
-        readNumber(number);
-    }
-    public static String read(int n) {
-        switch(n) {
-            case 1:
-                return "mot";
-            case 2:
-                return "hai";
-            case 3:
-                return "ba";
-            case 4:
-                return "bon";
-            case 5:
-                return "nam";
-            case 6:
-                return "sau";
-            case 7:
-                return "bay";
-            case 8:
-                return "tam";
-            default:return "chin";
-        }
-    }
-    public static void readNumber(int number) {
-      int number1=number/10;
-      int number2=number%10;
-        if (number2==0) {
-            if (number1==1) {
-                System.out.println("muoi");
-            }
-            else {
-                System.out.println(read(number1)+" muoi");
-            }
-        }
-        else {
-            if (number1!=1) {
-                System.out.println(read(number1)+" muoi "+read(number2));
-            }
-            else {
-                System.out.println("muoi"+" "+read(number2));
-            }
-            
-        }
+    public static void main(String[] args) 
+    {
+        int n;
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Nhap n:");
+        n=sc.nextInt();
+        gt2(n);
        
     }
+    public static int gt1(int n)
+    {
+        if (n==0)
+        {
+            return 1;
+        }
+        else
+        {
+            return n*gt1(n-1);
+        }
+    }
+   
+    public static void gt2(int n)
+    {
+        int p=1,i=1;
+        if (n==0)
+        {
+            System.out.println(1);
+        }
+        else
+        {
+        while(i<=n)
+        {
+            p=p*i;
+            i++;
+        }
+            System.out.println(p);
+        }
+    }
+    
     
 }
