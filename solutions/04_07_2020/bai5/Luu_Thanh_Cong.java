cd5879c94cb0a472aa9cd9b530af865a2ec3bb30
luuthanhcong <50271940+luuthanhcong@users.noreply.github.com>
2020-07-10 21:57:19 +0700

    Lưu Thành Công B5b

--- solutions/04_07_2020/bai5/Luu_Thanh_Cong.java
+++ solutions/04_07_2020/bai5/Luu_Thanh_Cong.java
@@ -11,19 +11,16 @@ import java.util.Scanner;
  *
  * @author luuthanhcong
  */
-public class B5a {
+public class B5b {
     public static void main(String[] args){
-        // s = 1 + 1/2 + 1/3 +...+ 1/n
         Scanner s = new Scanner(System.in);
-        int n;
-        float sum = 0;
+        int n = s.nextInt();
         System.out.println("Nhập n : ");
-        n = s.nextInt();
-        for (int i = 1; i <= n; i++) {
-            sum+= 1/i; 
+        int sum = 0;
+        for (int i = 1; i < n; i++) {
+            sum += i;
         }
         System.out.println(sum);
     }
-        
     
 }
