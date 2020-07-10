0440ced57ee9290852f25b0b5d7b4656a20a6822
phucdinh136 <62323993+phucdinh136@users.noreply.github.com>
2020-07-11 01:09:53 +0700

    b5.2

--- solutions/04_07_2020/bai5/Phucdinh.java
+++ solutions/04_07_2020/bai5/Phucdinh.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package bai5.pkg1;
+package bai5.pkg2;
 
 import java.util.Scanner;
 
@@ -11,19 +11,20 @@ import java.util.Scanner;
  *
  * @author linhb
  */
-public class BAI51 {
+public class BAI52 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
         double n, i, s = 0;
         Scanner input = new Scanner(System.in);
         System.out.println("Nhap n= ");
         n = input.nextDouble();
         for(i=1; i<=n;i++)
-            s  =  s+  1/i;
+            s  =  s+ i;
             System.out.println("giá trị của GT  là:" + s);
     }
     
