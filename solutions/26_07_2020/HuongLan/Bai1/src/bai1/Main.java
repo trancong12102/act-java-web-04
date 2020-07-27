20c1fa2eba9630398854f8f7e50b16192df54dcc
HuongLan <huongntl2000@gmail.com>
2020-07-27 13:12:28 +0700

    bai1

---
+++ solutions/26_07_2020/HuongLan/Bai1/src/bai1/Main.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bai1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Student
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        NhanVien nv1 = new NhanVien();
+        nv1.inputInfo();
+        nv1.printInfo();
+    }
+
+}
