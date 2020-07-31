1e28d040cfa0dabb4ae59beef946d701984d9707
soan27 <60722754+soan27@users.noreply.github.com>
2020-07-31 20:25:47 +0700

    hoangthixoan_bai3

---
+++ solutions/26_07_2020/Bai3/hoangthixoan_main3.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main311;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author iTplusHN
+ */
+public class Main311 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        Hocbong HB = new Hocbong();
+        HB.inputInfo();
+        HB.Kiemtra();
+    }
+    
+}
