9f88aa3c551996f59ed1db2de6198354f2bd02cd
tienduong-java <67788127+tienduong-java@users.noreply.github.com>
2020-07-31 22:07:12 +0700

    Đặng Tiến Dương nộp bafi5_main

---
+++ solutions/26_07_2020/Bai5/Đặng Tiến Dương_Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication4;
+
+/**
+ *
+ * @author Admin
+ */
+public class Main5 {
+    public static void main(String[] args) {
+        Triangle triangle1=new Triangle();
+        triangle1.inputInfo();
+        triangle1.Kieutamgiac();
+        triangle1.printInfo();
+    }
+    
+}
