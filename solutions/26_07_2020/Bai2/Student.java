800455731f6b342ea427f0084ae6affd80740a8e
katcorn <67743417+katcorn@users.noreply.github.com>
2020-08-02 14:41:26 +0700

    Bài 2 has been brought to you by Ngô Quang Đạt

--- solutions/26_07_2020/Bai2/Student.java
+++ solutions/26_07_2020/Bai2/Student.java
@@ -1,110 +1,96 @@
 
+package Object.Bai2;
+
 import java.util.Scanner;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- *
- * @author Administrator
- */
 public class Student {
-
     private String MSV;
     private float DTB;
-    private int age;
-    private String lop;
-
-    public Student() {
-    }
-
-    public Student(String MSV, float DTB, int age, String lop) {
+    private int Age;
+    private String Class;
+    
+    public Student(){}
+    
+    public Student(String MSV, float DTB, int Age, String Class){
         this.MSV = MSV;
         this.DTB = DTB;
-        this.age = age;
-        this.lop = lop;
+        this.Age = Age;
+        this.Class = Class;
     }
-
-    public String getMSV() {
+    String getMSV(){
         return MSV;
     }
-
-    public float getDTB() {
-        return DTB;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public String getLop() {
-        return lop;
-    }
-
-    public void setMSV(String MSV) {
+    void setMSV(String MSV){
         this.MSV = MSV;
     }
-
-    public void setDTB(float DTB) {
+    float getDTB(){
+        return DTB;
+    }
+    void setDTB(float DTB){
         this.DTB = DTB;
     }
-
-    public void setAge(int age) {
-        this.age = age;
+    int getAge(){
+        return Age;
     }
-
-    public void setLop(String lop) {
-        this.lop = lop;
+    void setAge(int Age){
+        this.Age = Age;
     }
-
-    public void inforINPUT() {
-        Scanner ip = new Scanner(System.in);
-        char a;
-        do {
-            System.out.print("nhap Ma Sinh vien=>");
-            this.MSV=ip.nextLine();
-        }while(MSV.length()!=8);
-        do
-        {
-            System.out.print("nhap diem trung binh=>");
-            this.DTB=ip.nextFloat();
-            ip.nextLine();
-        }while(this.DTB<0||this.DTB>10);
-        do
-        {
-            System.out.print("nhap tuoi=>");
-            this.age=ip.nextInt();
-            ip.nextLine();
-        }while(this.age<18);
-        do
-        {
-            System.out.print("nhap ten lop hoc=>");
-            this.lop=ip.nextLine();
-            a=lop.charAt(0);
-            
-        }while(a!='C' ||  a!='A');
+    String getclass(){
+        return Class;
     }
-    public void showINFOR()
-{
-    System.out.println("Ma sinh vien: "+this.MSV);
-    System.out.println("Lop: "+this.lop);
-    System.out.println("Diem trung binh: "+this.DTB);
-    System.out.println("Tuoi: "+this.age);
-
-}
-    public void kiemtraHB()
-    {
-        if(this.DTB>=8)
-        {
-            System.out.println("co hoc bong");
+    void setclass (String Class){
+        this.Class = Class;
+    }
+    void inputInfo(){
+        Scanner input = new Scanner(System.in);
+        for (int i = 0;i<=1;i++){
+            System.out.print("Nhập mã sinh viên: ");
+            MSV = input.nextLine();
+            if (MSV.length() != 8){
+                System.out.println("Mã sinh viên nhập sai, yêu cầu nhập lại!");
+                i = 0;
+            }
+        }    
+        for (int i = 0;i<=1;i++){
+            System.out.print("Nhập điểm trung bình của sinh viên: ");
+            DTB = input.nextFloat();
+            if (DTB < 0 || DTB > 10){
+                System.out.println("Điểm trung bình nhập sai, yêu cầu nhập lại!");
+                i = 0;
+            }
         }
+        for (int i = 0;i<=1;i++){
+            System.out.print("Nhập tuổi của sinh viên: ");
+            Age = input.nextInt();
+            input.nextLine();
+            if (Age < 18){
+                System.out.println("Tuổi sinh viên nhập sai, yêu cầu nhập lại!");
+                i = 0;
+            }
+        }
+        for (int i = 0;i<=1;i++){
+            System.out.print("Nhập lớp của sinh viên: ");
+            Class = input.nextLine();
+            if (Class.charAt(0)!='A'&& Class.charAt(0)!='C'){
+                System.out.println("Lớp của sinh viên nhập sai, yêu cầu nhập lại!");
+                i = 0;
+            }
+        }
+    }
+    void showInfo(){
+        System.out.println("Thông tin sinh viên: ");
+        System.out.println(" - Mã sinh viên: "+MSV);
+        System.out.println(" - Điểm trung bình: "+DTB);
+        System.out.println(" - Tuổi sinh viên: "+Age);
+        System.out.println(" - Sinh viên lớp: "+Class);
+        System.out.println(Scholarship());
+    }
+    String Scholarship(){
+        String Scholar;
+        if(DTB>8)
+            Scholar = " - Sinh viên được nhận học bổng";
         else
-        {
-            System.out.println("khong co hoc bong");
-        }
+            Scholar = " - Sinh viên không được nhận học bổng";
+        return Scholar;
     }
-
 }
-
---
+++ solutions/26_07_2020/Bai2/main.java
@@ -0,0 +1,10 @@
+
+package Object.Bai2;
+
+public class main {
+    public static void main(String[] args) {
+        Student st = new Student();
+        st.inputInfo();
+        st.showInfo();
+    }
+}
