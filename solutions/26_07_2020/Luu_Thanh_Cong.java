c75273bc85aa4ea7651250d0defe853522ab4b65
luuthanhcong <50271940+luuthanhcong@users.noreply.github.com>
2020-08-02 08:00:51 +0700

    Lưu Thành Công b5

--- solutions/26_07_2020/Luu_Thanh_Cong.java
+++ solutions/26_07_2020/Luu_Thanh_Cong.java
@@ -11,61 +11,62 @@ import java.util.Scanner;
  *
  * @author luuthanhcong
  */
-public class B4 {
-    private int a;
-
-    Scanner input = new Scanner(System.in);
-
-    public B4(int a) {
-        this.a = a;
-    }
-
-    B4() {
-    }
-
-    public int SoNguyenTo(int x) {
-        if (this.isSoNguyenTo(x)) {
-            this.a = x;
-        } else {
-            System.out.printf("%d khong phai la so nguyen to, khong luu tru !\n", x);
+public class B5 {
+      int a,b,c;
+        public B5(){}
+        public B5(int a, int b, int c){
+            this.a=a;
+            this.b=b;
+            this.c=c;
         }
-        return x;
-    }
 
-    boolean isSoNguyenTo(int x) {
-        if (x < 2) {
-            return false;
-        }
-        int squareRoot = (int) Math.sqrt(x);
-        for (int i = 2; i <= squareRoot; i++) {
-            if (x % i == 0) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    int timSoNguyenToTiepTheo() {
-        if (this.isSoNguyenTo(a)) {
-            for (int i = (a + 1); i < Integer.MAX_VALUE; i++) {
-                if (this.isSoNguyenTo(i)) {
-                    System.out.printf("so nguyen to lien ke sau a la %d\n", i);
-                    break;
-                }
-            }
-        }
+    public int getA() {
         return a;
     }
 
-    public int getSoNguyenTo() {
-        return this.a = a;
+    public void setA(int a) {
+        this.a = a;
     }
 
-    public void setSoNguyenTo(int a) {
-        if (isSoNguyenTo(a)) {
-            this.a = a;
-        } else {
-            System.out.println("khogn set");
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public int getC() {
+        return c;
+    }
+
+    public void setC(int c) {
+        this.c = c;
+    }
+    
+    void nhap(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Nhap vao do dai ba canh: ");
+        this.a=input.nextInt();
+        this.b=input.nextInt();
+        this.c=input.nextInt();
+    }
+    
+    void kiemtra(){
+        if(a==b && a==c && c==b){
+            System.out.println("Tam giac deu");
         }
+        if(a==b && a==c || b==a && b==c || c==a && c==b){
+            System.out.println("Tam giac can");
+        }
+        if(a*a == b*b + c*c || b*b == a*a + c*c || c*c == a*a + b*b){
+            System.out.println("Tam giac vuong");
+        }
+    }
+    
+    void tinhtoan(){
+        System.out.println("Chu vi cua tam giac la: " + (a+b+c)); 
+        double p=(a+b+c)/2;
+        System.out.println("Dien tich tam giac la: " +(double)Math.sqrt(p*(p-a)*(p-b)*(p-c)));
     }
 }
